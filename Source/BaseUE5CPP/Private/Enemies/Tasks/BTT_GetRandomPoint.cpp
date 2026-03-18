3f622f31611f4ee49546e41d5d9f6f5bb9132ca4
Bumdai <vanaslepcov861@gmail.com>
2026-03-18 22:49:01 +1000

    Alreadty point

--- Source/BaseUE5CPP/Private/Enemies/Tasks/BTT_GetRandomPoint.cpp
+++ Source/BaseUE5CPP/Private/Enemies/Tasks/BTT_GetRandomPoint.cpp
@@ -1,5 +1,8 @@
 #include "Enemies/Tasks/BTT_GetRandomPoint.h"
 #include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "NavigationSystem.h"
+#include "GameFramework/Pawn.h"
 
 UBTT_GetRandomPoint::UBTT_GetRandomPoint()
 {
@@ -15,5 +18,36 @@ EBTNodeResult::Type UBTT_GetRandomPoint::ExecuteTask(UBehaviorTreeComponent& Own
 		return EBTNodeResult::Failed;
 	}
 	
+	APawn* ControllerPawn = AIController->GetPawn();
+	if (!ControllerPawn)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ControllerPawn is NULL"));
+		return EBTNodeResult::Failed;
+	}
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp)
+	{
+		UE_LOG(LogTemp, Error, TEXT("BlackboardComp is NULL"));
+		return EBTNodeResult::Failed;
+	}
+	
+	FNavLocation RandomLocation;
+	const FVector Origin = ControllerPawn->GetActorLocation();
+	
+	UNavigationSystemV1* NavSystem = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+	if (!NavSystem)
+	{
+		UE_LOG(LogTemp, Error, TEXT("NavSystem is NULL"));
+		return EBTNodeResult::Failed;
+	}
+	
+	const bool bFoundPoint = NavSystem -> GetRandomReachablePointInRadius (Origin, SeachRadius, RandomLocation);
+	
+	BlackboardComp->SetValueAsVector(PatrolPointKey, RandomLocation.Location);
+	
+	UE_LOG(LogTemp, Warning, TEXT("BTT_GetRandomPoint: Patrol point set to %s"), *RandomLocation.Location.ToString());
+
 	return EBTNodeResult::Succeeded;
+	
 }
--- Source/BaseUE5CPP/Public/Enemies/Tasks/BTT_GetRandomPoint.h
+++ Source/BaseUE5CPP/Public/Enemies/Tasks/BTT_GetRandomPoint.h
@@ -12,5 +12,14 @@ class BASEUE5CPP_API UBTT_GetRandomPoint : public UBTTaskNode
 public:
 	UBTT_GetRandomPoint();
 
+protected:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float SeachRadius = 1000.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	FName PatrolPointKey = TEXT("PatrolPoint");
+	
 };
